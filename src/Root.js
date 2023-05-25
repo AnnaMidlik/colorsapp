9a4ad15f9c4e21177a4848808096a5ddff464ad3
AnnaMidlik <112081575+AnnaMidlik@users.noreply.github.com>
2023-05-25 17:10:13 +0300

    add pallettes initial state

--- src/Root.js
+++ src/Root.js
@@ -2,11 +2,16 @@ import React, { useEffect, useReducer } from 'react';
 import { PalettesContext } from './context/palettesContext';
 import { Outlet } from 'react-router-dom';
 import palettesReducer from './reducers/palettesReducer';
+import seedsColor from './data/seedsColor'
 
 export default function Root() {
   const [palettesState, palettesDispatch] = useReducer(palettesReducer, JSON.parse(localStorage.getItem('palettes')) || [])
   useEffect(() => {
-    localStorage.setItem('palettes', JSON.stringify(palettesState));
+    if (palettesState.length === 0) {
+      localStorage.setItem('palettes', JSON.stringify(seedsColor));
+    } else {
+      localStorage.setItem('palettes', JSON.stringify(palettesState));
+    }
   }, [palettesState])
   return (
     <PalettesContext.Provider
